ed10296e833c8160b68d1cf28fca38fc985f7f9c
maximfln <maxim-filon@mail.ru>
2021-05-11 01:45:44 +0300

    correct

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template, request, make_response, jsonify
 from markupsafe import Markup
-from components.index_page import Index_Page
+#from components.index_page import Index_Page
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return Markup.escape('<h1>hello world!</h1>')
+    return '<h1>hello world!</h1>'
